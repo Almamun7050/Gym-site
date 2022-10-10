a047ac25640d3815f70904ce67ae5a3515b1f034
Almamun7050 <105635038+Almamun7050@users.noreply.github.com>
2022-10-11 01:23:42 +0600

    gym website

---
+++ Module-44_rest-countries-recap/js/countries.js
@@ -0,0 +1,47 @@
+const loadCountries = ()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data =>displayCountries(data))
+}
+const displayCountries= countries =>{
+    console.log(countries);
+    const countriesHTML = countries.map(country =>getCountry(country));
+    // console.log(countriesHTML)
+    const container = document.getElementById('countries');
+    container.innerHTML=countriesHTML.join(' ');
+}
+// option -2
+const getCountry =( {name , flags, area, region}) =>{
+    return `
+        <div class="country">
+           <h2>${name.common}</h2>
+           <p>Area : ${area}</p>
+           <p>Continent: ${region}</p>
+           <img src="${flags.png}"> 
+        </div>
+    `
+}
+// option-1
+// const getCountry = country =>{
+    
+//         const {name , flags} = country
+    
+//         return `
+//             <div class="country">
+//                <h2>${name.common}</h2>
+//                <img src="${flags.png}"> 
+//             </div>
+//         `
+//     }
+
+//    original
+// const getCountry = country =>{
+//     return `
+//         <div class="country">
+//            <h2>${country.name.common}</h2>
+//            <img src="${country.flags.png}"> 
+//         </div>
+//     `
+// }
+
+loadCountries();
